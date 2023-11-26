9284eb9fd93e749be16a47d9622a93cb534fa4b6
AlmirNET <107294384+AlmirNet@users.noreply.github.com>
2023-11-26 20:54:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
-
-buttons.forEach((item) => {
-    item.onclick = () => {
-        if (item.id == "clear") {
-            display.innerText = "";
-        } else if (item.id == "backspace") {
-            let string = display.innerText.toString();
-            display.innerText = string.substr(0, string.length - 1);
-        } else if (display.innerText != "" && item.id == "equal") {
-            display.innerText = eval(display.innerText);
-        } else if (display.innerText == ""&& item.id == "equal") {
-            display.innerText = "Empty!";
-            setTimeout(() => (display.innerText = ""), 2000);
-        } else {
-            display.innerText += item.id;
-        }
-    };
-});
-
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
-const toggleIcon = document.querySelector(".toggle-icon");
-let isDark = true;
-themeToggleBtn.onclick = () => {
-    calculator.classList.toggle("dark");
-    themeToggleBtn.classList.toggle("active");
-    isDark = !isDark;
-};
 
+buttons.forEach((item) => {
+    item.addEventListener('click', () => {
+        handleButtonClick(item.id);
+    });
+});
+
+function handleButtonClick(buttonId) {
+    switch (buttonId) {
+        case 'clear':
+            clearDisplay();
+            break;
+        case 'backspace':
+            removeLastCharacter();
+            break;
+        case 'equal':
+            evaluateExpression();
+            break;
+        default:
+            appendToDisplay(buttonId);
+    }
+}
+
+function clearDisplay() {
+    display.innerText = '';
+}
+
+function removeLastCharacter() {
+    let string = display.innerText.toString();
+    display.innerText = string.substr(0, string.length - 1);
+}
+
+function evaluateExpression() {
+    if (display.innerText !== '') {
+        const result = calculate(display.innerText);
+        display.innerText = result;
+    } else {
+        display.innerText = 'Empty!';
+        setTimeout(() => (display.innerText = ''), 2000);
+    }
+}
+
+function calculate(expression) {
+    try {
+        return eval(expression);
+    } catch (error) {
+        return 'Error';
+    }
+}
+
+function appendToDisplay(value) {
+    display.innerText += value;
+}
+
+themeToggleBtn.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+    calculator.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+}
